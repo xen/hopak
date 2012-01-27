c0ebfceba88843f457ea679ec071dd1404e42cee
Denis Chernienko <denekurich@gmail.com>
2012-01-27 17:28:58 +0200

    moved test data files in sales

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,10 @@
+from formgear.models import Model, ModelRegistry
 
 def test():
-    pass 
+    class User(Model):
+        __yaml__ = 'tests/test/data/doctype.yaml'
+
+    class Order(Model):
+        __yaml__ = 'tests/test/sample/order.yaml'
+    
+    print ModelRegistry.list()
