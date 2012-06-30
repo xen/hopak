70505bab5242368412ada6104b341f11dd9665a8
xen <m@xen.ru>
2012-07-01 04:28:07 +0700

    обновил тесты

--- example.py
+++ example.py
@@ -10,16 +10,17 @@ class User(Model):
 
 john = User()
 
+print john.render_form()
 
-class Order(Model):
-    __yaml__ = 'order'
+#class Order(Model):
+#    __yaml__ = 'order'
 
-class All(Model):
-    __yaml__ = 'all'
+#class All(Model):
+#    __yaml__ = 'all'
 
 
 #order = Order()
 #print order.render_form()
 
-all = All()
-print all.render_form()
+#all = All()
+#print all.render_form()
